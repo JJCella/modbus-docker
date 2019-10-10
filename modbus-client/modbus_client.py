7c1b5dd6c6eeae49154bf889aacb77864d82dd32
JJCella <jeremy.cella@etud.univ-angers.fr>
2019-10-10 20:37:10 +0000

    first commit

---
+++ modbus-client/modbus_client.py
@@ -0,0 +1,35 @@
+from pyModbusTCP.client import ModbusClient
+import socket
+import time
+from datetime import datetime
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
+modbus_servers = []
+
+logging.info('Starting scanning')
+
+for i in range(1,101):
+    try:
+        host = 'modbus_server_{}'.format(i)
+        ip = socket.gethostbyname(host)
+    except:
+        pass
+    else:
+        modbus_servers.append({'host' : host, 'ip': ip, 'connection': None})
+
+for ms in modbus_servers:
+    ms['connection'] = ModbusClient(host=ip, port=5020)
+
+logging.info('{} host(s) found ! Starting reading loop'.format(len(modbus_servers)))
+
+while True:
+    for ms in modbus_servers:
+        c = ms['connection']
+        if c.is_open():
+           regs = c.read_holding_registers(0, 10)
+           logging.info('Regs read from {}'.format(ms['ip']))
+        else:
+           logging.info('Trying to connect to : {}'.format(ms['ip']))
+           c.open()
+    time.sleep(10)
